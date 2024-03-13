25b415528570720e3e59458cdab3ccfb79bbefd4
Rhina Kim <chihiroanihr@gmail.com>
2024-03-12 22:39:43 -0400

    Deleted envLoader.ts

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,8 +1,17 @@
-import { loadEnv, createConnString } from '@/utils';
+import dotenv from 'dotenv';
+import path from 'path';
+import { createConnString } from './utils';
 
 const loadConfig = () => {
   try {
-    loadEnv();
+    // Adjust the path as necessary based on your project structure
+    const envPath = path.resolve(__dirname, '../.env');
+    const result = dotenv.config({ path: envPath });
+    // Error
+    if (result.error) {
+      throw result.error;
+      // process.exit(1); // Optionally exit if the config is critical
+    }
     console.log('[*] .env file loaded successfully');
   } catch (error) {
     console.error('[!] Failed to load the .env file:', error);
--- packages/server/src/utils/index.ts
+++ packages/server/src/utils/index.ts
@@ -1,5 +1,2 @@
 export * from './generateRandomId';
 export * from './createConnString';
-
-import loadEnv from './envLoader';
-export { loadEnv };
